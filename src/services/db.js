4306facfe6c4df889c045b899a3abafc347de689
Guilherme Lima <guilhermelimak@gmail.com>
2017-07-07 02:34:13 -0300

    refactor: Cleaning unused shit and renaming other shit

--- src/services/db.js
+++ src/services/db.js
@@ -14,54 +14,54 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export default {
-  publish(contentHtml, contentMd) {
+  publishDocument(htmlValue, msValue) {
     const filesRef = database.ref().child('files').push();
-    const hashGenerated = hash();
-    const hashRef = database.ref().child(`hash/${hashGenerated}`);
+    const editKey = hash();
+    const hashRef = database.ref().child(`hash/${editKey}`);
 
-    filesRef.set({ contentHtml, contentMd });
+    filesRef.set({ htmlValue, msValue });
     hashRef.set(filesRef.key);
+
     return {
+      editKey,
       publishKey: filesRef.key,
-      editHash: hashGenerated,
     };
   },
-  save(contentHtml, contentMd, saveId) {
-    const filesRef = database.ref().child(`files/${saveId}`);
-    filesRef.update({ contentHtml, contentMd });
+  saveDocument(htmlValue, msValue, publishedId) {
+    const filesRef = database.ref().child(`files/${publishedId}`);
+    filesRef.update({ htmlValue, msValue });
   },
-  getEditContent(editKey) {
+  findDocumentByEditKey(editKey) {
     let contentKey = '';
-    const refHashUrl = database.ref(`hash/${editKey}`);
+    const hashUrlRef = database.ref(`hash/${editKey}`);
+
     return new Promise((resolve, reject) => {
-      refHashUrl.once('value', (hashSnapshot) => {
+      hashUrlRef.once('value', (hashSnapshot) => {
         contentKey = hashSnapshot.val();
-        const refContentUrl = database.ref(`files/${contentKey}`);
-        refContentUrl.once('value', (contentSnapshot) => {
-          if (!contentSnapshot || !contentSnapshot.val()) {
-            reject('Error');
-            return;
-          }
-          resolve({
-            htmlValue: contentSnapshot.val().contentHtml,
-            mdValue: contentSnapshot.val().contentMd,
-            publishKey: refContentUrl.key,
-            saveId: refContentUrl.key,
-            editKey,
-          });
+        const contentUrlRef = database.ref(`files/${contentKey}`);
+
+        contentUrlRef.once('value', (contentSnapshot) => {
+          if (!contentSnapshot || !contentSnapshot.val()) return reject('Error');
+
+          const { htmlValue, msValue } = contentSnapshot.val();
+          const publishKey = contentUrlRef.key
+
+          resolve({ htmlValue, mdValue, publishKey, editKey });
         });
       });
     });
   },
-  showSaved(publishedUrl) {
-    const ref = database.ref(`files/${publishedUrl}`);
+  findDocumentById(documentId) {
+    const ref = database.ref(`files/${documentId}`);
+
     return new Promise((resolve, reject) => {
+
       ref.once('value', (snapshot) => {
         if (!snapshot || !snapshot.val()) {
           reject('Error');
           return;
         }
-        resolve(snapshot.val().contentHtml);
+        resolve(snapshot.val().htmlValue);
       });
     });
   },
